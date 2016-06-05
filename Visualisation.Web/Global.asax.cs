f991709748ea45c800db1b4fc060d665902243e7
James Lappin <lappintheyoung@gmail.com>
2016-06-05 10:18:34 +0100

    Adding identy for mongo. It isnt working but want to be able to revert

--- Visualisation.Web/Global.asax.cs
+++ Visualisation.Web/Global.asax.cs
@@ -1,10 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Visualisation.Web
 {
-	public class MvcApplication : System.Web.HttpApplication
+	public class MvcApplication : HttpApplication
 	{
 		protected void Application_Start()
 		{
@@ -12,7 +13,6 @@ namespace Visualisation.Web
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
-			Bootstrapper.Initialise();
 		}
 	}
 }
