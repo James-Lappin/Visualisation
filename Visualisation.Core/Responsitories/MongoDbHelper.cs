bb61d93625e49a54376e8257006c244ed05fa560
James Lappin <lappintheyoung@gmail.com>
2016-07-18 23:06:23 +0100

    Adding dockerfile and updating configs to appropriate settings

--- Visualisation.Core/Responsitories/MongoDbHelper.cs
+++ Visualisation.Core/Responsitories/MongoDbHelper.cs
@@ -1,40 +1,66 @@
 using MongoDB.Driver;
+using System;
 using System.Configuration;
 
 namespace Visualisation.Core.Responsitories
 {
-    public class MongoDbHelper
-    {
-        public static IMongoDatabase GetDatabase()
-        {
-            var client = new MongoClient(GetConnectionString());
-            return client.GetDatabase(GetDatabaseName());
-        }
+	public class MongoDbHelper
+	{
+		public static IMongoDatabase GetDatabase()
+		{
+			var credential = MongoCredential.CreateCredential(GetDatabaseName(), GetUsername(), GetPassword());
+			var mongoClientSettings = new MongoClientSettings
+			{
+				Credentials = new[] { credential },
+				Server = new MongoServerAddress(GetDatabaseAddress(), GetDatabasePort()),
+			};
 
-        public static IMongoCollection<T> GetCollection<T>()
-        {
-            return GetCollection<T>(GetDatabase());
-        }
+			var client = new MongoClient(mongoClientSettings);
+			return client.GetDatabase(GetDatabaseName());
+		}
 
-        public static IMongoCollection<T> GetCollection<T>(IMongoDatabase database)
-        {
-            return database.GetCollection<T>(typeof(T).Name);
-        }
+		public static IMongoCollection<T> GetCollection<T>()
+		{
+			return GetCollection<T>(GetDatabase());
+		}
 
-        private static string GetConnectionString()
-        {
-            return ConfigurationManager
-                .AppSettings
-                .Get("MongoDbConnectionString")
-                .Replace("{DB_NAME}", GetDatabaseName());
-        }
+		public static IMongoCollection<T> GetCollection<T>(IMongoDatabase database)
+		{
+			return database.GetCollection<T>(typeof(T).Name);
+		}
 
-        private static string GetDatabaseName()
-        {
-            return ConfigurationManager
-                .AppSettings
-                .Get("MongoDbDatabaseName");
-        }
+		private static string GetDatabaseAddress()
+		{
+			return ConfigurationManager
+				.AppSettings
+				.Get("MongoDbAddress");
+		}
 
-    }
+		private static int GetDatabasePort()
+		{
+			return Convert.ToInt32(ConfigurationManager
+									.AppSettings
+									.Get("MongoDbPort"));
+		}
+
+		private static string GetDatabaseName()
+		{
+			return ConfigurationManager
+				.AppSettings
+				.Get("MongoDbName");
+		}
+
+		private static string GetUsername()
+		{
+			return ConfigurationManager
+				.AppSettings
+				.Get("MongoDbUsername");
+		}
+		private static string GetPassword()
+		{
+			return ConfigurationManager
+				.AppSettings
+				.Get("MongoDbPassword");
+		}
+	}
 }
---
+++ auth.sh
@@ -0,0 +1,43 @@
+
+#!/bin/bash
+
+if [ -f /.auth_setup_already ]; then
+	echo "MongoDB password already set!"
+	exit 0
+fi
+
+mongod --smallfiles --nojournal &
+
+PASS=${MONGODB_PASS:-$(pwgen -s 15 1)}
+_word=$( [ ${MONGODB_PASS} ] && echo "preset" || echo "random" )
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+echo "=> Creating an admin user with a ${_word} password in MongoDB"
+mongo admin --eval "db.createUser({user: 'admin', pwd: '$PASS', roles: [ 'root' ]});"
+mongo halo --eval "db.createUser({user: 'admin', pwd: '$PASS', roles: [ 'dbOwner' ]});"
+mongo remember --eval "db.createUser({user: 'admin', pwd: '$PASS', roles: [ 'dbOwner' ]});"
+
+mongo visualisation --eval "db.createUser({user: 'hideout', pwd: 'gettoglass', roles: [ 'dbOwner' ]})"
+mongo visualTesting --eval "db.createUser({user: 'testing', pwd: 'helloworld', roles: [ 'dbOwner' ]})"
+
+mongo admin --eval "db.shutdownServer();"
+
+echo "=> Done!"
+touch /.mongodb_password_set
+
+echo "========================================================================"
+echo "You can now connect to this MongoDB server using:"
+echo ""
+echo "    mongo admin -u admin -p $PASS --host <host> --port <port>"
+echo "    URI:"
+echo "    mongodb://admin:$PASS@<host>:<port>/<database>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
